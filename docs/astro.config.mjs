a29063bdd0e316d81d59bc2256d3ba2e108dd303
Swapnil Ogale <sswaogal@amazon.com>
2025-02-06 11:22:15 +1100

    docs: added nx docs sidebar

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -22,7 +22,52 @@ export default defineConfig({
           ],
         },
         {
+          label: 'Get started',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Quick start', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Tutorials',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Creating a new application', slug: 'guides/example' },
+            { label: 'Adding Nx plugin to your existing repo', slug: 'guides/example' },
+            { label: 'Customizing your generators', slug: 'guides/example' },
+            { label: 'Watch the videos', slug: 'guides/example' },
+          ],
+        },
+        {
           label: 'Reference',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Introduction', slug: 'guides/example' },
+            { label: 'Core concepts', slug: 'guides/example' },
+            { label: 'Generators', slug: 'guides/example' },
+            { label: 'Troubleshooting', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Contributing',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Reporting bugs', slug: 'guides/example' },
+            { label: 'Suggesting features', slug: 'guides/example' },
+            { label: 'Provide docs feedback', slug: 'guides/example' },
+            { label: 'How to contribute', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Blog',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Roadmap', slug: 'guides/example' },
+            { label: 'What\'s changed', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Tech Reference',
           autogenerate: { directory: 'reference' },
         },
       ],
