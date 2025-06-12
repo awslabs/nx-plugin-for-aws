b335bf9462ed6ef3905d20d22cfcc12448ee72c4
Marc Teichtahl <marc@teichtahl.com>
2025-06-12 15:31:19 +1000

    chore(website): introduce App inner component

--- packages/nx-plugin/src/cloudscape-website/cognito-auth/generator.spec.ts
+++ packages/nx-plugin/src/cloudscape-website/cognito-auth/generator.spec.ts
@@ -41,10 +41,13 @@ describe('cognito-auth generator', () => {
       import { RuntimeConfigProvider } from './components/RuntimeConfig';
       import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-      export function App() {
+      export function Main() {
+
+      const App = () => <RouterProvider router={router} />;
+
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
@@ -90,10 +93,13 @@ describe('cognito-auth generator', () => {
       import { RuntimeConfigProvider } from './components/RuntimeConfig';
       import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-      export function App() {
+      export function Main() {
+
+        const App = () => <RouterProvider router={router} />;
+        
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
@@ -147,10 +153,13 @@ describe('cognito-auth generator', () => {
       import { RuntimeConfigProvider } from './components/RuntimeConfig';
       import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-      export function App() {
+      export function Main() {
+
+        const App = () => <RouterProvider router={router} />;
+        
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
@@ -180,9 +189,11 @@ describe('cognito-auth generator', () => {
       import { RouterProvider, createRouter } from '@tanstack/react-router';
 
       export function App() {
+        const App = () => <RouterProvider router={router} />;
+        
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
@@ -210,9 +221,12 @@ describe('cognito-auth generator', () => {
       import { RouterProvider, createRouter } from '@tanstack/react-router';
 
       export function App() {
+
+        const App = () => <RouterProvider router={router} />;
+
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
@@ -233,11 +247,15 @@ describe('cognito-auth generator', () => {
       `
       import { RuntimeConfigProvider } from './components/RuntimeConfig';
       import { RouterProvider, createRouter } from '@tanstack/react-router';
+      
 
       export function App() {
+        
+        const App = () => <RouterProvider router={router} />;
+
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
@@ -446,9 +464,12 @@ export default AppLayout;
       import { RouterProvider, createRouter } from '@tanstack/react-router';
 
       export function App() {
+
+        const App = () => <RouterProvider router={router} />;
+
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
@@ -486,9 +507,12 @@ export default AppLayout;
       import { RouterProvider, createRouter } from '@tanstack/react-router';
 
       export function App() {
+
+      const App = () => <RouterProvider router={router} />;
+
         return (
           <RuntimeConfigProvider>
-            <RouterProvider router={router} />
+            <App/>
           </RuntimeConfigProvider>
         );
       }
