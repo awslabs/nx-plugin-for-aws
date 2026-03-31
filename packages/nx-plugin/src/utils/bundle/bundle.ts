5a8ade84b6b05cf9d87475ae5270816abe834f54
🚀 Jack <jacsteve@amazon.com>
2026-03-31 21:30:00 +1100

    fix: remove aws sdk tree shake workaround for rolldown bug now fixed
    Rolldown previously had a bug where tree shaking the AWS SDK credential provider would produce an
    invalid bundle and fail at runtime. We worked around this by disabling tree shaking for AWS SDK
    packages. This has since been fixed and we can remove the workaround.

--- packages/nx-plugin/src/utils/bundle/bundle.ts
+++ packages/nx-plugin/src/utils/bundle/bundle.ts
@@ -14,13 +14,7 @@ import {
 } from '@nx/devkit';
 import { withVersions } from '../versions';
 import { query, replace } from '../ast';
-import {
-  ArrayLiteralExpression,
-  factory,
-  ObjectLiteralExpression,
-  PropertyAssignment,
-  SyntaxKind,
-} from 'typescript';
+import { ArrayLiteralExpression, factory } from 'typescript';
 import { StringLiteral } from '@phenomnomnominal/tsquery';
 import { getRelativePathToRoot } from '../paths';
 import { addDependencyToTargetIfNotPresent } from '../nx';
@@ -137,14 +131,6 @@ export const addTypeScriptBundleTarget = (
 ) => {
   project.targets ??= {};
 
-  // Check if AWS SDK is external
-  const awsSdkRegex = /@aws-sdk\/.*/;
-  const isAwsSdkExternal = opts.external?.some((ext) =>
-    ext instanceof RegExp
-      ? ext.source === awsSdkRegex.source
-      : ext === awsSdkRegex.source,
-  );
-
   // Generate empty rolldown config if it doesn't exist
   generateFiles(
     tree,
@@ -178,39 +164,6 @@ export const addTypeScriptBundleTarget = (
 
   const rolldownConfigArraySelector =
     'CallExpression:has(Identifier[name="defineConfig"]) > ArrayLiteralExpression';
-
-  // Add disableTreeShake function if AWS SDK is not external and it doesn't exist
-  // This is due to an issue with rolldown bundling for the SDK, and can likely be removed when resolved
-  // https://github.com/rolldown/rolldown/issues/6513
-  if (!isAwsSdkExternal) {
-    const disableTreeShakeSelector =
-      'VariableDeclaration:has(Identifier[name="disableTreeShake"])';
-    if (
-      query(tree, rolldownConfigPath, disableTreeShakeSelector).length === 0
-    ) {
-      const content = tree.read(rolldownConfigPath, 'utf-8');
-      const disableTreeShakeFunction = `// Disables tree-shaking for the given module patterns
-const disableTreeShake = (patterns: RegExp[]) => ({
-  name: 'disable-treeshake',
-  transform: (code, id) => {
-    if (patterns.some(p => p.test(id))) {
-      return { code, map: null, moduleSideEffects: 'no-treeshake' };
-    }
-    return null;
-  },
-});
-
-`;
-      const defineConfigIndex = content.indexOf('export default defineConfig');
-      if (defineConfigIndex !== -1) {
-        const newContent =
-          content.slice(0, defineConfigIndex) +
-          disableTreeShakeFunction +
-          content.slice(defineConfigIndex);
-        tree.write(rolldownConfigPath, newContent);
-      }
-    }
-  }
 
   // Check whether we already have a config entry with input set to targetFilePath
   if (
@@ -285,26 +238,6 @@ const disableTreeShake = (patterns: RegExp[]) => ({
                               ),
                         ),
                       ),
-                    ),
-                  ]
-                : []),
-              ...(!isAwsSdkExternal
-                ? [
-                    factory.createPropertyAssignment(
-                      factory.createIdentifier('plugins'),
-                      factory.createArrayLiteralExpression([
-                        factory.createCallExpression(
-                          factory.createIdentifier('disableTreeShake'),
-                          undefined,
-                          [
-                            factory.createArrayLiteralExpression([
-                              factory.createRegularExpressionLiteral(
-                                `/${awsSdkRegex.source}/`,
-                              ),
-                            ]),
-                          ],
-                        ),
-                      ]),
                     ),
                   ]
                 : []),
